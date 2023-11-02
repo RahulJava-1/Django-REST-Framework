e9aee6e63f5b28fc1bf9441f6d0737146538c41b
RahulJava-1 <rahultripathirudra@gmail.com>
2023-11-03 00:38:35 +0530

    get and post API

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',index,name='index'),
+    path('post_todo/', post_todo, name='post_todo'),
+    path('get_todo/',get_todo,name='get_todo'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,79 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-
+from .serializers import TodoSerializers
+from .models import Todo
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST', 'PATCH'])
+
 def index(request):
-    return Response(
-        {
-            'status':200,
-            'message':'DRF'
-        }
-    )
+    if request.method == "POST":
+        return Response(
+            {
+                'status':200,
+                'message':'DRF',
+                'Method_called':"You Called POST method"
+
+            }
+        )
+    elif request.method == "GET":
+        return Response(
+            {
+                'status':'200',
+                'message':'Smoothly run',
+                'Method_called':'GET'
+            }
+        )
+    elif request.method == 'PATCH':
+        return Response({
+            'status':'200',
+            'message':'Fantastic PATCH',
+            'method_called':'PATCH'
+        })
+    else:
+        return Response({
+            'status':'400',
+            'message':'ERROR',
+            'method_called':'Unknown Method'
+        })
+
+@api_view(['GET'])
+def get_todo(request):
+    todo_objs = Todo.objects.all()
+    serializer = TodoSerializers(todo_objs, many=True)
+
+    return Response({
+        'status':True,
+        'message':'fetched',
+        'data':serializer.data
+    })
+
+
+
+@api_view(['POST'])
+def post_todo(request):
+    try:
+        data = request.data
+        serializer = TodoSerializers(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            print(serializer.data)
+            return Response({
+                'status':True,
+                'message': 'Correct',
+                'data': serializer.data
+            })
+
+        else:
+            return Response({
+            'status': False,
+            'message':'Kindly fill the required fields',
+            'data': serializer.errors
+        })
+    except Exception as e:
+        print(e)
+
+    return Response({
+        'status': False,
+        'message':'something went wrong'
+    })       
